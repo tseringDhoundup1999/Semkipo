033d1bec60e2b80376918c69e674c9a65a48d964
Tsering dhoundup1999 <tseringlama9813694977@gmail.com>
2026-05-07 09:12:41 +0545

    fix(database) remove SSL mode options from database settings

--- core/settings.py
+++ core/settings.py
@@ -141,11 +141,7 @@ DATABASES = {
         "NAME": "semkipoc_semkipo_db",
         "USER": "semkipoc_semkipo_user",
         "PASSWORD": "Dhoundup1999@",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-          "OPTIONS": {
-            "sslmode": "require",
-        },
+       
     }
 }
 

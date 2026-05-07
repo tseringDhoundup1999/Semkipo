15acfe9208fa317c95eb4f5e0b1804b72483a7e5
Tsering dhoundup1999 <tseringlama9813694977@gmail.com>
2026-05-07 08:35:31 +0545

    feat(database) update database settings to use PostgreSQL and add requirements.txt

--- core/settings.py
+++ core/settings.py
@@ -128,10 +128,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "semkipoc_semkipo_db",
+        "USER": "semkipoc_semkipo_user",
+        "PASSWORD": "your_password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 

8cf8b2dbd8f31bd0a1d4026c56e03518c94786a1
tseringDhoundup1999 <tseringlama9813694977@gmail.com>
2026-05-07 06:37:46 +0545

    add the static file root

--- core/settings.py
+++ core/settings.py
@@ -172,6 +172,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = BASE_DIR / "media" 
 MEDIA_URL = "/media/" 
